48fbe69df8bac182c12bb1887b12b914d015cac9
sn <serhii.nv@gmail.com>
2025-11-19 14:04:34 +0000

    added flipCard top and bottom

--- components/FlipCard.tsx
+++ components/FlipCard.tsx
@@ -15,7 +15,7 @@ export default function FlipCard({front, back}: FlipCardProps) {
             sx={{
                 perspective: "1200px",
                 minHeight: 400,
-                minWidth: {xs:"100%", md: "800px"},
+                minWidth: {xs: "100%", md: "800px"},
                 maxWidth: 600,
                 // maxHeight: 500,
                 margin: "0 auto",
@@ -86,36 +86,55 @@ export default function FlipCard({front, back}: FlipCardProps) {
                 >
                     {back}
                 </Box>
-            </Box>
-            {/* Flip indicator overlay */}
-            <Box
-                className="flip-indicator"
-                sx={{
-                    position: "absolute",
-                    left: "50%",
-                    bottom: 10,
-                    transform: "translateX(-50%)",
-                    // bgcolor: "primary.light",
-                    bgcolor: flipped ? "primary.dark" : "background.paper",
-                    color: flipped ? "background.paper" : "primary.dark",
-                    // color: "#fff",
-                    borderRadius: 2,
-                    px: 1,
-                    py: 0.5,
-                    display: "flex",
-                    alignItems: "center",
-                    gap: 0,
-                    fontSize: 14,
-                    pointerEvents: "none",
-                    transition: "transform 0.5s,",
-                    zIndex: 10,
-                    userSelect: "none",
-                }}
-            >
-                <AutorenewIcon fontSize="small" sx={{mr: 1}}/>
-                <Typography variant="body2" sx={{fontWeight: 500}}>
-                    Flip
-                </Typography>
+                {/* Flip indicator overlay */}
+                <Box
+                    className="flip-indicator"
+                    sx={{
+                        position: "absolute",
+                        bottom: 0,
+                        width: "100%",
+                        bgcolor: flipped ? "primary.dark" : "background.paper",
+                        color: flipped ? "background.paper" : "primary.dark",
+                        borderRadius: 3,
+                        px: 0,
+                        py: 0.7,
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "center",
+                        gap: 1,
+                        fontSize: 14,
+                        pointerEvents: "none",
+                        // transition: "transform 0.5s",
+                        transition: "background-color 0.6s cubic-bezier(0.4, 0, 0.2, 1), color 0.6s cubic-bezier(0.4, 0, 0.2, 1)",
+
+                        zIndex: 10,
+                        userSelect: "none",
+                        textAlign: "center",
+                        transform: `${flipped ? " rotateY(180deg)" : ""}`,
+
+                    }}
+                >
+                    <AutorenewIcon fontSize="small" sx={{mr: 1}}/>
+                    <Typography variant="body2" sx={{fontWeight: 500}}>
+                        Flip
+                    </Typography>
+                </Box>
+                {/* Flip top overlay */}
+                <Box
+                    className="flip-indicator"
+                    sx={{
+                        position: "absolute",
+                        top: 0,
+                        width: "100%",
+                        bgcolor: flipped ? "primary.dark" : "background.paper",
+                        borderRadius: 3,
+                        px: 0,
+                        py: 1.7,
+                        display: "flex",
+                        transition: "background-color 0.6s cubic-bezier(0.4, 0, 0.2, 1), color 0.6s cubic-bezier(0.4, 0, 0.2, 1)",
+                        zIndex: 10,
+                    }}
+                />
             </Box>
         </Box>
     );
